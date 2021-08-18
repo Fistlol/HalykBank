8507d0fc3ce2103472adf609963408a92bb4e131
Fistlol <64380003+Fistlol@users.noreply.github.com>
2021-08-19 03:09:11 +0600

    Simple App on C#

---
+++ simpleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace simpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            float first_number = float.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            float second_number = float.Parse(Console.ReadLine());
+
+            Console.Write("Сумма двух чисел равна = ");
+            Console.WriteLine(first_number + second_number);
+            Console.Write("Разность двух чисел равна = ");
+            Console.WriteLine(first_number - second_number);
+            Console.Write("Произведение двух чисел равна = ");
+            Console.WriteLine(first_number * second_number);
+            Console.Write("Частное двух чисел равна = ");
+            Console.WriteLine(first_number / second_number);
+        }
+    }
+}
